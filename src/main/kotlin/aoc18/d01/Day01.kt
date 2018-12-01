a511e8b9309488800c469d5e4a238dae56996de5
Shemnei <Shemnei@users.noreply.github.com>
2018-12-01 12:09:54 +0100

    solved day 01

--- src/main/kotlin/aoc18/d01/Day01.kt
+++ src/main/kotlin/aoc18/d01/Day01.kt
@@ -3,18 +3,26 @@ package aoc18.d01
 import aoc18.util.AoC
 import aoc18.util.Day
 import aoc18.util.MyAoCSettings
+import aoc18.util.cycle
 
-object Day01 : Day(2017) {
-    override fun runOne(input: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+object Day01 : Day<List<Int>>() {
 
-    override fun runTwo(input: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun prepare(input: String): List<Int> = input.lines().map { it.toInt() }
+
+    override fun taskOne(input: List<Int>) = input.sum().toString()
+
+    override fun taskTwo(input: List<Int>): String {
+        val seenFrequencies = mutableSetOf<Int>()
+        var freq = 0
+        return input.cycle().map {
+            seenFrequencies += freq
+            freq += it
+            freq
+        }.first { it in seenFrequencies }.toString()
     }
 }
 
 fun main(args: Array<String>) {
     AoC.settings = MyAoCSettings
-    Day01.runOne("")
+    Day01.runLogging()
 }
